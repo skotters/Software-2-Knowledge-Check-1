f97bdc58ed99fd05891c0642d149bc0b7dce9a9a
David Paller <dpaller94@gmail.com>
2022-07-26 22:01:25 -0400

    add code

---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/City.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class City
+    {
+        public int ZipCode { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public int Population { get; set; }
+
+        public List<Building> Buildings { get; } = new List<Building>();
+
+        public void CreateApartment(Apartment apartment)
+        {
+            Buildings.Add(apartment);
+        }
+
+        public void CreateHighRise(HighRise highRise)
+        {
+            Buildings.Add(highRise);
+        }
+    }
+}

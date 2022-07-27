f1e003f2cc9778ff0f87dc55311266800489a076
David Paller <dpaller94@gmail.com>
2022-07-26 22:21:45 -0400

    updated code

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/City.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/City.cs
@@ -17,12 +17,44 @@ namespace Software2KnowledgeCheck1
 
         public void CreateApartment(Apartment apartment)
         {
-            Buildings.Add(apartment);
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                Buildings.Add(apartment);
+            }
         }
 
-        public void CreateHighRise(HighRise highRise)
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T: Building
         {
-            Buildings.Add(highRise);
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+        
     }
 }
